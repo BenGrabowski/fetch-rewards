6a5cb79ff57e7dcdd1a9ebc39ec2454c3a815757
Ben Grabowski <gbowski30@gmail.com>
2020-12-02 18:21:36 -0700

    added STORE

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class App extends Component {
+  state = {
+      list: undefined
+  };
+
+  componentDidMount() {
+      fetch('https://fetch-hiring.s3.amazonaws.com/hiring.json',
+        {
+            method: 'GET',
+            // mode: 'no-cors'
+        }
+      )
+      .then(res => res.json())
+      .then(data => {
+          this.setState({ list: data});
+      })
+      .catch(error => console.log(error));
+  }
+  
+  render() {
+    return (
+        <main className='App'>
+          <h1>Hello World</h1>
+        </main>
+      );    
+  }
 }
 
 export default App;
